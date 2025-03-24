aa4af9c9529e37a92ac323b7003f2f8d19e9c42f
2011kwj <kim01234jin@jbnu.ac.kr>
2025-03-24 10:05:45 +0900

    message

---
+++ Project2/Project2/소스.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void hanoi(int n, int start, int work, int target);
+
+void main()
+{
+	hanoi(10, 'A', 'B', 'c');
+	getchar();
+
+}
+void hanoi(int n, int start, int work, int target)
+{
+	if (n == 1)
+		printf("%c에서 원반%d을 %c로 옮김\n", start, n, target);
+	else {
+		hanoi(n - 1, start, target, work);
+		printf("%c에서 원반%d를 %c로 옮김\n", start,target,work);
+		hanoi(n - 1,work,start, target);
+	}
+}
